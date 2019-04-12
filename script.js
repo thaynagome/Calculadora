68c417cd0c9ccbc46f8f653d1f32b5d0a1ee4d6f
Thayna Gomes da Luz Santos <1802222@SALAS.AULAS>
2019-04-12 19:41:31 -0300

    parte do js

---
+++ script.js
@@ -0,0 +1,94 @@
+$( function(){
+
+    var v1, v2, op;
+
+    $("input[name=btn]").click( function(){
+        $("#result").val($("#result").val() + $(this).val());
+    });
+
+    $("input[name=ce]").click( function(){
+        $("#result").val('');
+        $("#op").val('');
+    });
+
+    $("input[name=ce]").click( function(){
+        $("#result").val('');
+    });
+
+    $("input[name=soma]").click( function(){
+        if($("#result").val() != ''){
+            v1 = parseFloat($("#result").val());
+            $("result").val('');
+            op="soma";
+            $("#op").text($(this).val());
+        }else{
+            alert('Insira um valor para efetuar a operação!');
+        }
+    });
+
+    $("input[name=menos]").click( function(){
+        if($("#result").val() != ''){
+            v1 = parseFloat($("#result").val());
+            $("result").val('');
+            op="menos";
+            $("#op").text($(this).val());
+        }else{
+            alert('Insira um valor para efetuar a operação!');
+        }
+    });
+
+    $("input[name=mult]").click( function(){
+        if($("#result").val() != ''){
+            v1 = parseFloat($("#result").val());
+            $("result").val('');
+            op="mult";
+            $("#op").text($(this).val());
+        }else{
+            alert('Insira um valor para efetuar a operação!');
+        }
+    });
+
+    $("input[name=div]").click( function(){
+        if($("#result").val() != ''){
+            v1 = parseFloat($("#result").val());
+            $("result").val('');
+            op="div";
+            $("#op").text($(this).val());
+        }else{
+            alert('Insira um valor para efetuar a operação!');
+        }
+    });
+
+    $("input[name=igual]").click( function(){
+        if($("#result").val() != ''){
+            v2 = parseFloat($("#result").val());
+           
+            if(op == "soma"){
+                $("#result").val(v1 + v2);
+            }
+
+            else if(op == "menos"){
+                $("#result").val(v1-v2);
+            }
+           
+            else if(op == "vezes"){
+                $("#result").val(v1*v2);
+            }
+
+            else{
+                $("#result").val(v1/v2);
+            }
+
+        }else{
+            alert('Insira um valor para efetuar o cálculo!');
+        }
+    });
+
+    $("input[name=c]").click( function(){
+        var len = $("#result").val().length;
+
+        var valor = $("#result").val();
+
+        valor = valor.replace(valor.charAt(len-1), "");
+    });
+});
